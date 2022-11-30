ade6656cf965b89806ecda58f427f049b66442b8
Morning <morning@mstars-lab3-08s-iMac.local>
2022-11-30 10:27:37 +0800

    js string

---
+++ string.js
@@ -0,0 +1,32 @@
+//Task1
+let firstname="byambasuren";
+let lastname="bayarsaikhan";
+if(firstname.length > lastname.length)
+    console.log("ner ni urt");
+else{
+    console.log("ovog ni urt");
+}
+
+//Task2
+const str = "Text messages are used for personal, family, business and social purposes. Governmental and non-governmental organizations use text messaging for communication between colleagues. ";
+console.log(str.toUpperCase());
+
+//Task3
+const string = "Text messages are used for personal, family, business and social purposes. Governmental and non-governmental organizations use text messaging for communication between colleagues. ";
+console.log(string.toLowerCase());
+
+//Task4
+let a = "pinecone academy-н";
+let b = a.concat(' ','leap хөтөлбөрт тавтай морилно уу');
+console.log(b);
+
+//Task5
+let fname = "Byambasuren";
+let country = "ulaanbaatar hot";
+let city = "bayanzurh duuregt";
+
+let str1 = "Namaig \r" + fname + "\r gedeg " + country + "iin \r" + city +"\r amidardag";
+console.log(str1);
+
+//Task6
+console.log("1 2 3 4 5 \n2 3 4 5 1 \n3 4 5 1 2 \n4 5 1 2 3 \n5 1 2 3 4");
